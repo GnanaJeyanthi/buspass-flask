8d0da092b594275042ae02957bec41e9ce1a6ddd
GnanaJeyanthi <jeyanthi282005@gmail.com>
2026-01-12 09:30:02 +0530

    Describe your changes

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'jeyanthi282005@gmail.com'  # Replace with your email
-app.config['MAIL_PASSWORD'] = 'efpk puwx zzrs wusp'          # Replace with your app password
+app.config['MAIL_PASSWORD'] = 'irkm edgu tong cqbg'          # Replace with your app password
 
 db = SQLAlchemy(app)
 mail = Mail(app)
